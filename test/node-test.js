ff0f4a389bb660314f1089c168baff9b420fcfb3
KK578 <KK1241@googlemail.com>
2016-08-20 20:26:26 +0100

    Split node test suite from app test suite.

---
+++ test/node-test.js
@@ -0,0 +1,35 @@
+﻿const path = require('path');
+const helpers = require('yeoman-test');
+const assert = require('yeoman-assert');
+
+describe('yo kk578:node MyNodeProject', () => {
+	const dummies = [
+		[helpers.createDummyGenerator(), 'kk578:app']
+	];
+
+	before(() => {
+		return helpers.run(path.join(__dirname, '../generators/node'))
+			.withGenerators(dummies)
+			.inDir(path.join(__dirname, './tmp/node'))
+			.withArguments('MyNodeProject')
+			.withPrompts({
+				name: 'The Tester',
+				email: 'tester@test.test'
+			})
+			.toPromise();
+	});
+
+	it('should generate package.json with MyProject', () => {
+		assert.file('package.json');
+		assert.fileContent('package.json', /"name": "MyNodeProject"/);
+	});
+
+	it('should generate grunt files', () => {
+		assert.file('gruntfile.js');
+		assert.file(['configs/grunt/']);
+	});
+
+	it('should generate misc. config files', () => {
+		assert.file(['.eslintrc.json']);
+	});
+});
