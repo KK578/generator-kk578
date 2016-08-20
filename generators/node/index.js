2ec14107dcf01e51ef4be7ea43f29b76f510a29a
KK578 <KK1241@googlemail.com>
2016-08-20 20:34:48 +0100

    Add initial generator, composing with main app generator.

---
+++ generators/node/index.js
@@ -0,0 +1,9 @@
+﻿const generators = require('yeoman-generator');
+
+const generator = generators.Base.extend({
+	initializing() {
+		this.composeWith('kk578:app');
+	}
+});
+
+module.exports = generator;
