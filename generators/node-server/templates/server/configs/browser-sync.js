3968e57d62d2e9c66e9b8503f70ed663b2e8b507
KK578 <KK1241@googlemail.com>
2016-08-22 16:55:59 +0100

    Add browser-sync script to server.

---
+++ generators/node-server/templates/server/configs/browser-sync.js
@@ -0,0 +1,33 @@
+﻿module.exports = (server) => {
+	if (server.get('env') === 'production') {
+		return;
+	}
+
+	const browserSync = require('browser-sync');
+	const bs = browserSync.create('Server');
+
+	// Defer browser-sync initialisation until next tick to ensure the express server is running.
+	process.nextTick(() => {
+		const port = process.env.BROWSER_SYNC_PORT || 3000;
+		const proxy = `localhost:${parseInt(server.get('port'))}`;
+		const files = [
+			{
+				match: [
+					'public/**/*.{html,css,js}'
+				]
+			}
+		];
+
+		bs.init({
+			files: files,
+			logPrefix: 'BrowserSync',
+			minify: true,
+			open: false,
+			reloadOnRestart: true,
+			reloadDebound: 2000,
+			server: false,
+			port: port,
+			proxy: proxy
+		});
+	});
+};
--- generators/node-server/templates/server/server.js
+++ generators/node-server/templates/server/server.js
@@ -2,6 +2,7 @@
 const logs = require('./configs/logs.js');
 const setup = require('./configs/setup.js');
 const router = require('./configs/router.js');
+const browserSync = require('./configs/browser-sync.js');
 
 module.exports = () => {
 	const server = express();
@@ -10,6 +11,7 @@ module.exports = () => {
 	logs(server);
 	setup(server);
 	router(server);
+	browserSync(server);
 	// End configuration.
 
 	return server;
