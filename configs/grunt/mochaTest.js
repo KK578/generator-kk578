7ed647f025681828dee8295b3a0976354f3f2162
KK578 <KK1241@googlemail.com>
2016-08-22 01:24:44 +0100

    Enforce order of tests when running all.

--- configs/grunt/mochaTest.js
+++ configs/grunt/mochaTest.js
@@ -1,6 +1,12 @@
 ﻿module.exports = {
 	options: { reporter: 'spec' },
-	all: { src: 'test/*-test.js' },
+	all: {
+		src: [
+			'test/app-test.js',
+			'test/node-test.js',
+			'test/node-server-test.js'
+		]
+	},
 	app: { src: 'test/app-test.js' },
 	node: { src: 'test/node-test.js' },
 	nodeServer: { src: 'test/node-server-test.js' }
