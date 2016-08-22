da3024325050b8cfc28a6cc4f82ee4c2f6d097b0
KK578 <KK1241@googlemail.com>
2016-08-22 14:07:24 +0100

    Setup render engine and gzip compression.

--- generators/node-server/templates/server/server.js
+++ generators/node-server/templates/server/server.js
@@ -1,11 +1,13 @@
 ﻿const express = require('express');
 const logs = require('./configs/logs.js');
+const setup = require('./configs/setup.js');
 
 module.exports = () => {
 	const server = express();
 
 	// Run configuration scripts here.
 	logs(server);
+	setup(server);
 	// End configuration.
 
 	return server;
