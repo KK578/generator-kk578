431f67900fb16b2dbbf0ff9d9412b9b2e1446c45
KK578 <KK1241@googlemail.com>
2016-08-30 00:37:22 +0100

    Refactor node-server-generator to use helper functions.

--- generators/node-server/index.js
+++ generators/node-server/index.js
@@ -7,51 +7,14 @@ const generator = generators.Base.extend({
 	// Cannot use arrow notation due to this object not referencing the correct object.
 	constructor: function () {
 		generators.Base.apply(this, arguments);
-
-		this.argument('appName', {
-			type: String,
-			description: 'Name for your application',
-			required: false,
-			optional: true
-		});
-
-		util.prompts.nodeServer.map((p) => {
-			this.option(p.name);
-		});
+		util.generatorConstructor.call(this, util.prompts.nodeServer);
 	},
 	initializing() {
 		const done = this.async();
-
-		if (this.appName) {
-			this.options.appName = this.appName;
-		}
-
-		util.checkForGit(path.join(process.cwd(), '.git'), (git) => {
-			this.git = git;
-			done();
-		});
+		util.generatorInitializing.call(this, done);
 	},
 	prompting() {
-		const requiredPrompts = [];
-
-		util.prompts.node.map((p) => {
-			// Check that this hasn't been enabled already as an option.
-			if (this.options[p.name] === undefined) {
-				// Bind the current git remote to default.
-				if (p.name === 'gitRemoteUrl') {
-					p.default = this.git.remoteUrl;
-				}
-
-				requiredPrompts.push(p);
-			}
-		});
-
-		return this.prompt(requiredPrompts)
-			.then((answers) => {
-				requiredPrompts.map((p) => {
-					this.options[p.name] = answers[p.name];
-				});
-			});
+		return util.generatorPrompting.call(this, util.prompts.nodeServer);
 	},
 	composition() {
 		this.options.nodeServer = true;
