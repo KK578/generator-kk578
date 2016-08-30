84679fead9f0d56ad4311c7b06f258d5232d798c
KK578 <KK1241@googlemail.com>
2016-08-30 23:08:33 +0100

    Add polymer-app test suite to grunt.

--- grunt/mochaTest.js
+++ grunt/mochaTest.js
@@ -4,10 +4,12 @@
 		src: [
 			'test/app-test.js',
 			'test/node-test.js',
-			'test/node-server-test.js'
+			'test/node-server-test.js',
+			'test/polymer-app-test.js'
 		]
 	},
 	app: { src: 'test/app-test.js' },
 	node: { src: 'test/node-test.js' },
-	'node-server': { src: 'test/node-server-test.js' }
+	'node-server': { src: 'test/node-server-test.js' },
+	'polymer-app': { src: 'test/polymer-app-test.js' }
 };
