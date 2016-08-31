b0f23b5f9ca48a64a15087742b0abb23f92cfda6
KK578 <KK1241@googlemail.com>
2016-08-31 22:12:27 +0100

    Add browser-sync custom plugin loading.

--- generators/node-server/templates/server/configs/browser-sync.js
+++ generators/node-server/templates/server/configs/browser-sync.js
@@ -1,4 +1,26 @@
-﻿module.exports = (server) => {
+﻿const fs = require('fs');
+const path = require('path');
+
+function loadPlugins(bs, callback) {
+	fs.readdir(path.join(__dirname, 'browser-sync/plugins'), (err, files) => {
+		if (err) {
+			return callback(err);
+		}
+
+		files.map((file) => {
+			const plugin = require(file);
+
+			bs.use({
+				plugin: plugin.plugin,
+				hooks: plugin.hooks
+			});
+		});
+
+		callback();
+	});
+}
+
+module.exports = (server) => {
 	if (server.get('env') === 'production') {
 		return;
 	}
@@ -6,28 +28,30 @@
 	const browserSync = require('browser-sync');
 	const bs = browserSync.create('Server');
 
-	// Defer browser-sync initialisation until next tick to ensure the express server is running.
-	process.nextTick(() => {
-		const port = process.env.PORT_BROWSER_SYNC || 3000;
-		const proxy = `localhost:${parseInt(server.get('port'))}`;
-		const files = [
-			{
-				match: [
-					'public/**/*.{html,css,js}'
-				]
-			}
-		];
+	loadPlugins(bs, () => {
+		// Defer browser-sync initialisation until next tick to ensure express server is running.
+		process.nextTick(() => {
+			const port = process.env.PORT_BROWSER_SYNC || 3000;
+			const proxy = `localhost:${parseInt(server.get('port'))}`;
+			const files = [
+				{
+					match: [
+						'public/**/*.{html,css,js}'
+					]
+				}
+			];
 
-		bs.init({
-			files: files,
-			logPrefix: 'BrowserSync',
-			minify: true,
-			open: false,
-			reloadOnRestart: true,
-			reloadDebound: 2000,
-			server: false,
-			port: port,
-			proxy: proxy
+			bs.init({
+				files: files,
+				logPrefix: 'BrowserSync',
+				minify: true,
+				open: false,
+				reloadOnRestart: true,
+				reloadDebound: 2000,
+				server: false,
+				port: port,
+				proxy: proxy
+			});
 		});
 	});
 };
---
+++ generators/node-server/templates/server/configs/browser-sync/plugins/scroll.js
@@ -0,0 +1,18 @@
+﻿// TODO: Confirm the event name.
+/* eslint no-var: "off", prefer-template: "off" */
+window.addEventListener('WebComponentsReady', function () {
+	var elements = ['#mainContainer'];
+
+	elements.map(function (elementName) {
+		var element = document.querySelector(elementName);
+		var eventName = elementName + ':scroll';
+
+		element.addEventListener('scroll', function (e) {
+			window.___browserSync___.socket.emit(eventName, e.target.scrollTop);
+		});
+
+		window.___browserSync___.socket.on(eventName, function (scrollTop) {
+			element.scrollTop = scrollTop;
+		});
+	});
+});
