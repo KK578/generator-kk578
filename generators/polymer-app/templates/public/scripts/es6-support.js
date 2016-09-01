f0586c77d13ad41651aa458cc705cef5a00d8bbc
KK578 <KK1241@googlemail.com>
2016-09-02 00:11:00 +0100

    Add index.html and loading scripts.

---
+++ generators/polymer-app/templates/public/scripts/es6-support.js
@@ -0,0 +1,26 @@
+﻿/* eslint no-var: "off" */
+(function () {
+	function supportsEs6() {
+		try {
+			// Attempt to evaluate an arrow function definition.
+			// Must do from within a string to avoid a SyntaxError.
+			eval('var x = (y) => (y + 1);');
+		}
+		catch (e) {
+			return false;
+		}
+
+		return true;
+	}
+
+	var script = document.createElement('script');
+
+	if (supportsEs6()) {
+		script.src = 'scripts/load.js';
+	}
+	else {
+		script.src = 'scripts/load.es5.js';
+	}
+
+	document.head.appendChild(script);
+})();
---
+++ generators/polymer-app/templates/public/scripts/load.js
@@ -0,0 +1,70 @@
+﻿(function () {
+	const loadingDelay = 100;
+	const finishDelay = 500;
+
+	function endSplashScreen() {
+		document.getElementById('splash-screen').runAnimation();
+	}
+
+	function loadAppDependencies() {
+		Polymer.Base.importHref('elements.html', function () {
+			const loaders = document.querySelectorAll('.loading');
+
+			loaders.forEach((loader) => {
+				loader.classList.remove('loading');
+			});
+
+			// HACK: Some browsers don't appear to like multiple animations running on the same
+			// element at the same time. So run async to allow loading class styles to be
+			// removed before seaming the card into the background.
+			window.setTimeout(function () {
+				// End splash-screen animation.
+				document.getElementById('splash-spinner').active = false;
+				document.getElementById('splash-card').elevation = 0;
+			}, finishDelay);
+
+			window.setTimeout(endSplashScreen, finishDelay);
+		});
+	}
+
+	function startSplashScreen() {
+		// Scale in paper-spinner and animate paper-material.
+		document.getElementById('splash-spinner').classList.remove('loading');
+		document.getElementById('splash-card').elevation = 5;
+
+		window.setTimeout(loadAppDependencies, loadingDelay);
+	}
+
+	function loadInitialDependencies() {
+		function loaded() {
+			window.setTimeout(startSplashScreen, 0);
+		}
+
+		const elementImport = document.createElement('link');
+
+		elementImport.rel = 'import';
+		elementImport.href = 'splash_elements.html';
+		elementImport.onload = loaded;
+
+		document.head.appendChild(elementImport);
+	}
+
+	function checkNativeWebComponents() {
+		const native = 'registerElement' in document &&
+			'import' in document.createElement('link') &&
+			'content' in document.createElement('template');
+
+		if (native) {
+			loadInitialDependencies();
+		}
+		else {
+			const script = document.createElement('script');
+
+			script.src = 'bower-components/webcomponentsjs/webcomponents-lite.js';
+			script.onload = loadInitialDependencies;
+			document.head.appendChild(script);
+		}
+	}
+
+	checkNativeWebComponents();
+})();
