a50ab00adec9317fb38252542666398b61dc6e45
KK578 <KK1241@googlemail.com>
2016-09-01 02:33:58 +0100

    Add component dev route.

---
+++ generators/polymer-app/templates/server/routes/components.dev.js
@@ -0,0 +1,40 @@
+﻿const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const router = express.Router();
+
+
+// Stamp css content into html during development.
+router.get('/custom-components/:component/*.html', (req, res) => {
+	const element = req.params.component;
+	const elementPath = path.join(__dirname, `../../public/${element}/${element}`);
+
+	fs.readFile(`${elementPath}.html`, { encoding: 'utf-8' }, function (err, html) {
+        if (err) {
+            throw err;
+        }
+
+		fs.readFile(`${elementPath}.css`, { encoding: 'utf-8' }, function (err, css) {
+			if (err) {
+				throw err;
+			}
+
+			// Find the style tag that references the component's css.
+			const stampLocation = new RegExp(`<style.*href=".*${element}\.css".*?><\/style>`);
+			const taggedCss = `<style>${css}</style>`;
+
+			html.replace(stampLocation, taggedCss)
+		});
+	});
+});
+
+// Don't send the css file if it has been requested directly.
+router.get('/custom-components/**/*.css', (req, res) => {
+	res.send('');
+});
+
+
+module.exports = {
+	mountPath: '/',
+	router: router
+};
