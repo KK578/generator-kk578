4cad59249ebeb9855152a36d852785c363708226
KK578 <KK1241@googlemail.com>
2016-09-03 21:39:35 +0100

    Add prompts to utilities.

---
+++ util/prompts.js
@@ -0,0 +1,39 @@
+﻿const prompts = {
+	all: [
+		{
+			type: 'input',
+			name: 'appName',
+			message: 'Your project name'
+		}
+	]
+};
+
+prompts.app = prompts.all.concat([
+	{
+		type: 'input',
+		name: 'name',
+		message: 'Your name',
+		store: true
+	},
+	{
+		type: 'input',
+		name: 'email',
+		message: 'Your email address',
+		store: true
+	},
+	{
+		type: 'input',
+		name: 'gitRemoteUrl',
+		message: 'Remote Git repository URL',
+		optional: true
+	}
+]);
+
+// Node
+prompts.node = prompts.app;
+prompts.nodeServer = prompts.node;
+
+// Polymer
+prompts.polymerApp = prompts.nodeServer;
+
+module.exports = prompts;
--- util/util.js
+++ util/util.js
@@ -3,5 +3,6 @@
 	generator: require('generator.js'),
 	git: require('./git.js'),
 	gruntConfigs: require('./grunt-configs.js'),
-	packageJson: require('./package-json.js')
+	packageJson: require('./package-json.js'),
+	prompts: require('./prompts.js')
 };
