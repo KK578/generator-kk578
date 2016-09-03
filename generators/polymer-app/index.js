d2091e94ca46fc57b813930537f25abe608e5794
KK578 <KK1241@googlemail.com>
2016-09-03 22:02:57 +0100

    Rework polymer-app-generator to use new util.

--- generators/polymer-app/index.js
+++ generators/polymer-app/index.js
@@ -1,64 +1,35 @@
 ﻿const generators = require('yeoman-generator');
 
-const util = require('../util.js');
+const util = require('../../util/util.js');
+const prompts = util.prompts.polymerApp;
+
+let bowerJson;
 
 const generator = generators.Base.extend({
 	// Cannot use arrow notation due to this object not referencing the correct object.
 	constructor: function () {
 		generators.Base.apply(this, arguments);
-		util.generatorConstructor.call(this, util.prompts.polymerApp);
+		util.generator.constructor.call(this, prompts);
 	},
 	initializing() {
 		const done = this.async();
 
-		util.generatorInitializing.call(this, done);
+		util.generator.initializing.call(this, done);
 	},
 	prompting() {
-		return util.generatorPrompting.call(this, util.prompts.polymerApp);
-	},
-	bowerJson() {
-		const bowerJson = {
-			name: this.options.appName,
-			version: '0.0.0',
-			ignore: [
-				'**/.*',
-				'node_modules',
-				'bower_components',
-				'test',
-				'tests'
-			],
-			authors: [{
-				name: this.options.name,
-				email: this.options.email
-			}],
-			license: 'BSD-3-Clause',
-			dependencies: {
-				'lodash': '^4.15.0',
-				'polymer': 'Polymer/polymer#^1.6.1',
-				'paper-elements': 'PolymerElements/paper-elements#^1.0.7'
-			},
-			devDependencies: {
-				'iron-component-page': 'PolymerElements/iron-component-page#^1.1.7'
-			}
-		};
-
-		if (this.options.gitRemoteUrl) {
-			bowerJson.repository = {
-				type: 'git',
-				url: this.options.gitRemoteUrl
-			};
-		}
-
-		this.options.bowerJson = bowerJson;
+		return util.generator.prompting.call(this, prompts);
 	},
 	composition() {
 		this.options.polymerApp = true;
 		this.composeWith('kk578:node-server', { options: this.options });
 	},
+	bowerJson() {
+		bowerJson = util.bowerJson.create(this.options);
+	},
 	writing() {
 		/* eslint max-statements: "off" */
 		this.copy('.bowerrc');
-		this.write('bower.json', JSON.stringify(this.options.bowerJson, null, 2));
+		this.write('bower.json', JSON.stringify(bowerJson, null, 2));
 
 		this.copy('server/routes/bower.dev.js');
 		this.copy('server/routes/components.dev.js');
