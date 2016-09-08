1e8e83235fc7c96cc66f8b032d0414b76f0193f1
KK578 <KK1241@googlemail.com>
2016-09-08 02:31:38 +0100

    Add grunt htmllint config.

---
+++ grunt/htmllint.js
@@ -0,0 +1,15 @@
+﻿module.exports = {
+	options: {
+		htmllintrc: true,
+		force: true
+	},
+	'polymer-app': {
+		files: [
+			{
+				expand: true,
+				cwd: 'generators/polymer-app/templates/public/',
+				src: ['**/*.html']
+			}
+		]
+	}
+};
